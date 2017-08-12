7af3c8d5e395a10464137947b6ebb260c1f0310b
wenjayliu <865015063@qq.com>
2017-08-12 19:02:59 +0800

    :construction: 监听文件修改启动server
    $ cnpm run gulp server
    
    编译less
    browser-sync  存在问题

--- gulpfile.js
+++ gulpfile.js
@@ -19,3 +19,37 @@ gulp.task('create-directory', () => {
   }
 });
 //在终端运行cnpm run gulp create-directory
+
+/*全局定义要处理的文件*/
+const files = {
+  lessFiles: './src/less/go.less',
+  cssFiles: './src/css/*.css',
+  jsFiles: './src/js/*.js',
+  imgFiles:'./src/img/*.*'
+}
+//编译less
+gulp.task('compile-less', () => {
+const less = require('gulp-less'); //依赖gulp-less的插件
+const notify = require('gulp-notify'); 
+const plumber = require('gulp-plumber');
+const browserSync = require('browser-sync').create(); //browser-sync同步服务器
+const reload = browserSync.reload; //将browser-sync的reload方法存起来，方便调用
+  return gulp.src(files.lessFiles)
+  .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })) //使用gulp-notify和gulp-plumber用来阻止因为less语法写错跳出监视程序发生
+  .pipe(less())
+  .pipe(gulp.dest(dirs.css + '/'))
+  .pipe(reload({stream: true}));
+});
+
+// 本地服务器功能，自动刷新（开发环境）
+gulp.task('server', ['compile-less'],()=>{
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+  browserSync.init({
+    server: './'
+  });
+  gulp.watch(dirs.less+'/**/*.less', ['compile-less']); //监视less文件夹中的所有less文件，有改动就调用compile-less任务编译less
+  gulp.watch('./*.html').on('change', reload); //监视html文件，有改动就刷新浏览器
+  gulp.watch(dirs.js+'/**/*.js').on('change', reload); //监视所有js文件有改动就刷新浏览器
+});
+//在cmd运行cnpm run gulp server
