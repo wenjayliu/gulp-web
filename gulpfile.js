021d9a3131fa120cb6e80fabf2031494157e95f8
wenjayliu <865015063@qq.com>
2017-08-12 21:25:27 +0800

    :bug:  重构gulp
    https://github.com/NicknameID/gulp-flow.git
    
    https://github.com/NicknameID/gulp-flow

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
-//gulpfile.js
-/*首先在全局上加载gulp，这个很重要*/
 const gulp = require('gulp');
-/*在全局上定义项目的目录结构，供应后面使用*/
+const path = require('path');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+const less = require('gulp-less');
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
+const runSequence = require('gulp-sequence').use(gulp);
+/*项目的目录结构*/
 const dirs = {
   dist:'./dist',
   src: './src',
@@ -10,49 +15,55 @@ const dirs = {
   js: './src/js',
   img: './src/img',
 };
-gulp.task('create-directory', () => {
-  const mkdirp = require('mkdirp'); //这里要依赖mkdirp这个包，已通过cnpm package.json安装
-  for (let i in dirs) {
-    mkdirp(dirs[i], err => {
-      err ? console.log(err) : console.log('mkdir-->' + dirs[i]);;
-    });
-  }
-});
-//在终端运行cnpm run gulp create-directory
 
-/*全局定义要处理的文件*/
+/*要处理的文件名*/
 const files = {
   lessFiles: './src/less/go.less',
   cssFiles: './src/css/*.css',
   jsFiles: './src/js/*.js',
   imgFiles:'./src/img/*.*'
 }
+
+// ------------------开发阶段------------------------------------------------------
+gulp.task('start', ['create-directory']); //项目初始化的第一个命令
+gulp.task('dev-watch', ['server']); //开始编写项目后开启服务器实时更新
+
+// ------------------生产阶段------------------------------------------------------
+gulp.task('prefixer', ['autoprefixer']); //给css文件添加浏览器私有前缀 files.cssFiles ==>> .src/css/
+gulp.task('min-css', ['minify-css']); //压缩css文件 files.cssFiles ==>> dist/css/
+gulp.task('js-handl', ['js-concat-compress']); //合并计算文件  dirs.js/**/*.js ==>> ./dist/js/concated.js
+
+//-------------------一键生成项目文件-----------------------------------------------
+
+gulp.task('bunld-project',runSequence('clean-dist','compile-less','autoprefixer','minify-css','js-concat-compress','img-handl','zip'))
+
+
+/*_________________上面为配置项_____________________________________________________*/
+/*1、生成项目目录*/
+gulp.task('create-directory', () => {
+  const mkdirp = require('mkdirp'); //创建文件夹
+  for (let i in dirs) {
+    mkdirp(dirs[i], err => {
+      err ? console.log(err) : console.log('mkdir-->' + dirs[i]);;
+    });
+  }
+});
+
+gulp.task('clean-dist',()=>{
+  const clean = require('gulp-clean');
+  return gulp.src('./dist', {read: false}).pipe(clean());
+});
+
+/*2、css处理*/
 //编译less
 gulp.task('compile-less', () => {
-const less = require('gulp-less'); //依赖gulp-less的插件
-const notify = require('gulp-notify'); 
-const plumber = require('gulp-plumber');
-const browserSync = require('browser-sync').create(); //browser-sync同步服务器
-const reload = browserSync.reload; //将browser-sync的reload方法存起来，方便调用
   return gulp.src(files.lessFiles)
-  .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })) //使用gulp-notify和gulp-plumber用来阻止因为less语法写错跳出监视程序发生
+  .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
   .pipe(less())
   .pipe(gulp.dest(dirs.css + '/'))
   .pipe(reload({stream: true}));
 });
 
-// 本地服务器功能，自动刷新（开发环境）
-gulp.task('server', ['compile-less'],()=>{
-const browserSync = require('browser-sync').create();
-const reload = browserSync.reload;
-  browserSync.init({
-    server: './'
-  });
-  gulp.watch(dirs.less+'/**/*.less', ['compile-less']); //监视less文件夹中的所有less文件，有改动就调用compile-less任务编译less
-  gulp.watch('./*.html').on('change', reload); //监视html文件，有改动就刷新浏览器
-  gulp.watch(dirs.js+'/**/*.js').on('change', reload); //监视所有js文件有改动就刷新浏览器
-});
-//在cmd运行cnpm run gulp server
 
 //添加浏览器私有前缀（生产环境）
 gulp.task('autoprefixer', () => {
@@ -60,8 +71,8 @@ gulp.task('autoprefixer', () => {
   const sourcemaps = require('gulp-sourcemaps');
   const autoprefixer = require('autoprefixer');
   return gulp.src(files.cssFiles)
-    .pipe(sourcemaps.init()) //添加sourcemap,方便调试
-    .pipe(postcss([ autoprefixer() ])) //添加浏览器私有前缀，解决浏览器的兼容问题
+    .pipe(sourcemaps.init())
+    .pipe(postcss([ autoprefixer() ]))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(dirs.css+'/'))
 });
@@ -72,22 +83,33 @@ gulp.task('minify-css', function () {
   const rename = require("gulp-rename");
   return gulp.src(dirs.css+'/**/*.css')
     .pipe(minifyCSS({/*keepBreaks: true*/}))
-    .pipe(rename(path=>path.basename += '.min')) //重命名文件输出后的样式为 原文件名.min.css
+    .pipe(rename(path=>path.basename += '.min'))
     .pipe(gulp.dest('./dist/css/'))
+});
+
+
+// 本地服务器功能，自动刷新（dev）
+gulp.task('server', ['compile-less'],()=>{
+  browserSync.init({
+    server: './'
+  });
+  gulp.watch(dirs.less+'/**/*.less', ['compile-less']);
+  gulp.watch('./*.html').on('change', reload);
+  gulp.watch(dirs.js+'/**/*.js').on('change', reload);
 });
 
 // js文件--合并--压缩(生产环境)
 gulp.task('js-concat-compress', (cb)=>{
-  let name = ''; //先定义一个变量将用于后面存文件名
+  let name = '';
   const concat = require('gulp-concat');
   const uglify = require('gulp-uglify');
   const rename = require("gulp-rename");
   return gulp.src(dirs.js+'/**/*.js')
   .pipe(rename(path=>{path.basename += '';name=path.basename;}))
-  .pipe(concat('bundle.js'))   //合并js文件
-  .pipe(uglify())         //压缩js文件
+  .pipe(concat('bundle.js'))//合并后的文件名
+  .pipe(uglify())
   .pipe(rename(path=>{
-    path.basename = name+'.'+path.basename+'.min';  //改文件名加上 .min
+    path.basename = name+'.'+path.basename+'.min';
   }))
   .pipe(gulp.dest('dist/js/')); 
 });
@@ -96,31 +118,14 @@ gulp.task('js-concat-compress', (cb)=>{
 gulp.task('img-handl',()=>{
   const imagemin = require('gulp-imagemin');
   return gulp.src(files.imgFiles)
-    .pipe(imagemin())  //imagemin()里是可以写参数的，有需要的可以去github的页面看看
-    .pipe(gulp.dest('./dist/img/'))
+		.pipe(imagemin())
+		.pipe(gulp.dest('./dist/img/'))
 });
 
 // 项目打包(生产环境)
 gulp.task('zip',()=>{
   const zip = require('gulp-zip');
-  return gulp.src(['./*.html','**/dist/**/*.*','!**/node_modules/**/*.*']) //这里需要注意的是，在写要打包的文件时，避免打包的文件不能写在开头，这里'!**/node_modules/**/*.*'放在了最后
-  .pipe(zip('project.zip'))   //打包后的文件名，自己随意取
+  return gulp.src(['./*.html','**/dist/**/*.*','!**/node_modules/**/*.*'])
+  .pipe(zip('project.zip'))
   .pipe(gulp.dest('./'))
 });
-
-
-
-// ------------------开发阶段命令----------------------------------------------------
-gulp.task('start', ['create-directory']); //项目初始化的第一个命令
-gulp.task('dev-watch', ['server']); //开始编写项目后开启服务器实时更新
-
-// ------------------生产阶段命令------------------------------------------------------
-gulp.task('prefixer', ['autoprefixer']); //给css文件添加浏览器私有前缀 files.cssFiles ==>> .src/css/
-gulp.task('min-css', ['minify-css']); //压缩css文件 files.cssFiles ==>> dist/css/
-gulp.task('js-handl', ['js-concat-compress']); //合js文件  dirs.js/**/*.js ==>> ./dist/js/concated.js
-gulp.task('img-handl', ['img-handl']) //处理图片，对图片进行无损的压缩
-
-//----------------一键生成项目文件命令-----------------------------------------------
-       //因为gulp执行任务时是以最大的任务并发数同时进行的，所以有时候我们需要按步骤进行，就需要插件`gulp-sequence`，将任务按顺序写入，就会按顺序执行
-const runSequence = require('gulp-sequence').use(gulp);
-gulp.task('bunld-project',runSequence('clean-dist','compile-less','autoprefixer','minify-css','js-concat-compress','img-handl','zip'))
